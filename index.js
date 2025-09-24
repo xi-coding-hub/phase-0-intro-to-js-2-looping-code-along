836ee91a704d91cc48a43e3e9699a3ec3d205ad7
el-sam <samurai.h@yahoo.com>
2025-09-24 21:12:26 +0800

    1st function pass

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    // debugger;
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+
+
+
+const names = ["Alo","Ali","Lili"]
+let message = [ ]
+function writeCards(names) {
+    
+    for (let i=0; i<names.length; i++) {
+      let response =  `Thank you, ${names[i]}, for the wonderful surprise gift!` ;
+      // debugger;
+      message.push(response);
+    }
+    // console.log(message);
+    return message;
+}
