415e2b312da5146dc06489b8bf4c6ebf7d5b637e
el-sam <samurai.h@yahoo.com>
2025-09-25 11:32:07 +0800

    test completed

--- index.js
+++ index.js
@@ -1,27 +1,37 @@
-const gifts = ["teddy bear", "drone", "doll"];
+// const gifts = ["teddy bear", "drone", "doll"];
 
-function wrapGifts(gifts) {
-  for (let i = 0; i < gifts.length; i++) {
-    console.log(`Wrapped ${gifts[i]} and added a bow!`);
-    // debugger;
-  }
+// function wrapGifts(gifts) {
+//   for (let i = 0; i < gifts.length; i++) {
+//     console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//     // debugger;
+//   }
 
-  return gifts;
-}
+//   return gifts;
+// }
 
-wrapGifts(gifts);
+// wrapGifts(gifts);
 
 
 
 const names = ["Alo","Ali","Lili"]
 let message = [ ]
 function writeCards(names) {
-    
-    for (let i=0; i<names.length; i++) {
+    let count =0;
+    for (let i = 0; i < names.length; i++) {
       let response =  `Thank you, ${names[i]}, for the wonderful surprise gift!` ;
       // debugger;
       message.push(response);
+      count += 1;
+      console.log(count); 
     }
     // console.log(message);
     return message;
 }
+
+function countDown(num) {
+  for (let i = num; i >= 0; i--) {
+    console.log(i);
+    
+  }
+}
+
